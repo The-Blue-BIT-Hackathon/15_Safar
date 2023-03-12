8cb3346184250c63d076ef29827b1364877e8c4c
TMR <chetandagajipatil333@gmail.com>
2023-03-12 15:01:33 +0530

    payment done

--- app/src/main/java/com/safar/pccoehackathon/customer/CustomerMessInfoActivity.java
+++ app/src/main/java/com/safar/pccoehackathon/customer/CustomerMessInfoActivity.java
@@ -3,10 +3,17 @@ package com.safar.pccoehackathon.customer;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -16,13 +23,19 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.safar.pccoehackathon.R;
 
+import java.util.ArrayList;
+
 public class CustomerMessInfoActivity extends AppCompatActivity {
 
     private String email;
     private FirebaseFirestore firebaseFirestore;
     private LinearLayout llData;
-    private TextView tvMessName, tvLocation, tvPhoneNumber, tvEmail;
+    private TextView tvMessName, tvLocation, tvPhoneNumber, tvEmail, tvUPI;
+    private Button btnMakePayment;
 
+    String GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
+    int GOOGLE_PAY_REQUEST_CODE = 123;
+    final int PAY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +48,22 @@ public class CustomerMessInfoActivity extends AppCompatActivity {
         tvLocation = findViewById(R.id.tvLocation);
         tvPhoneNumber = findViewById(R.id.tvPhoneNumber);
         tvEmail = findViewById(R.id.tvEmail);
+        tvUPI = findViewById(R.id.tvUPI);
+
+        btnMakePayment = findViewById(R.id.btnMakePayment);
 
         llData = findViewById(R.id.llData);
 
         setHeader(email);
 
         getDishes(email);
+
+        btnMakePayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                payUsingUpi();
+            }
+        });
 
     }
 
@@ -147,12 +170,99 @@ public class CustomerMessInfoActivity extends AppCompatActivity {
                         String location = value.getString("location");
                         String phoneNumber = value.getString("ownerphone");
                         String email = value.getString("email");
+                        String upi = value.getString("upi");
 
                         tvMessName.setText(messName);
                         tvLocation.setText(location);
                         tvPhoneNumber.setText(phoneNumber);
                         tvEmail.setText(email);
+                        tvUPI.setText(upi);
                     }
                 });
     }
+
+    private void payUsingUpi() {
+
+        Uri uri =
+                new Uri.Builder()
+                        .scheme("upi")
+                        .authority("pay")
+                        .appendQueryParameter("pa", tvUPI.getText().toString().trim())
+                        .appendQueryParameter("pn", tvMessName.getText().toString().trim())
+                        .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
+
+
+        Intent chooser = Intent.createChooser(intent, "Pay With");
+
+        if (null != chooser.resolveActivity(CustomerMessInfoActivity.this.getPackageManager())) {
+            startActivityForResult(chooser, PAY_REQUEST);
+        } else {
+            Toast.makeText(CustomerMessInfoActivity.this, "No Upi Id Found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PAY_REQUEST) {
+            if (isInternetAvailable(CustomerMessInfoActivity.this)) {
+                if (data == null) {
+                    ArrayList<String> dataList = new ArrayList<>();
+                    dataList.add("nothing");
+                    String temp = "nothing";
+                    Toast.makeText(CustomerMessInfoActivity.this, "Transaction not complete", Toast.LENGTH_SHORT).show();
+                } else {
+                    String text = data.getStringExtra("response");
+                    ArrayList<String> dataList = new ArrayList<>();
+                    dataList.add(text);
+
+                    upiPaymentCheck(text);
+                }
+            }
+        }
+
+    }
+
+    void upiPaymentCheck(String data) {
+        String str = data;
+        String payment_cancel = "";
+        String status = "";
+        String response[] = str.split("&");
+
+        for (int i = 0; i < response.length; i++) {
+            String equalStr[] = response[i].split("");
+            if (equalStr.length >= 2) {
+                if (equalStr[0].toLowerCase().equals("Status".toLowerCase())) {
+                    status = equalStr[1].toLowerCase();
+                }
+            } else {
+                payment_cancel = "Payment Cancel";
+            }
+
+            if (status.equals("success")) {
+                Toast.makeText(CustomerMessInfoActivity.this, "Transaction Successfully", Toast.LENGTH_SHORT).show();
+            } else if ("Payment Cancel".equals(payment_cancel)) {
+                Toast.makeText(CustomerMessInfoActivity.this, "Payment Cancel by user", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(CustomerMessInfoActivity.this, "Transaction Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public static boolean isInternetAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo.isConnected() && networkInfo.isConnectedOrConnecting() && networkInfo.isAvailable()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
